885f09626ac39f0a0e08c61a74ba2186810b11fe
Marco <scavuzzo.marco@gmail.com>
2014-10-06 12:18:18 +0200

    Adding proper Maven build plugin

---
+++ src/main/java/it/polimi/hegira/EntryClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.polimi.hegira;
+
+/**
+ * @author Marco Scavuzzo
+ *
+ */
+public class EntryClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
