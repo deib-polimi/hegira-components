01102d85e21531f54538d09d0254e2f43aba0323
Andrea_Celli <andrea.celli@mail.polimi.it>
2015-04-13 11:16:32 +0200

    udpates in the Table class

--- src/main/java/it/polimi/hegira/adapters/cassandra/Table.java
+++ src/main/java/it/polimi/hegira/adapters/cassandra/Table.java
@@ -1,5 +1,11 @@
 package it.polimi.hegira.adapters.cassandra;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 /**
  * This class manages a single Table.
  * Used to check columns, alter the table if necessary, and perform inserts into Cassandra.
@@ -9,8 +15,21 @@ package it.polimi.hegira.adapters.cassandra;
 public class Table {
 
 	private String tableName;
+	private List<String> columns;
 	
 	public Table(String tableName){
 		this.tableName=tableName;
+		columns=new ArrayList<String>();
+	}
+	
+	/**
+	 * This methods inserts a row in the table.
+	 * This methods is synchronized in order to avoid conflicts due to different threads changing the same table.
+	 * 
+	 * @param row - the row to be insterted
+	 * @param session - the session of the current thread
+	 */
+	public synchronized void insert(Row row,Session session){
+		
 	}
 }
