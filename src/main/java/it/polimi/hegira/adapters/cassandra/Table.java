d1b0d1bf4d9f4c8da98176c6c0f0ee7772312b15
Andrea_Celli <andrea.celli@mail.polimi.it>
2015-04-13 10:13:43 +0200

    added class for Table Management in the cassandra adapter

---
+++ src/main/java/it/polimi/hegira/adapters/cassandra/Table.java
@@ -0,0 +1,11 @@
+package it.polimi.hegira.adapters.cassandra;
+
+/**
+ * This class manages a single Table.
+ * Used to check columns, alter the table if necessary, and perform inserts into Cassandra.
+ * @author Andrea Celli
+ *
+ */
+public class Table {
+
+}
---
+++ src/main/java/it/polimi/hegira/adapters/cassandra/TablesManager.java
@@ -0,0 +1,12 @@
+package it.polimi.hegira.adapters.cassandra;
+
+/**
+ * This class implements the singleton pattern so that all the threads share the same instance of the Table Manager.
+ * The Table Manager manages Tables instances and assign them to threads when needed.
+ * 
+ * @author Andrea Celli
+ *
+ */
+public class TablesManager {
+
+}
